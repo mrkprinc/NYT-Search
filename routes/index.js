b835d79db1d8688422133acfe9cab966daf9c97e
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-22 15:02:05 -0400

    routes fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const apiRoutes = require('./apiRoutes');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  res.send('api ready...')
 })
 
 module.exports = router;
