19569794f0759d7c89a80f3a0950f79187afdad8
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-23 22:18:04 -0400

    savepoint

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+  next();
 }  
 app.use(allowCrossDomain); // plumbing it in as middleware
 
