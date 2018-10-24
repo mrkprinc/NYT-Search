95bd64b610a1cc9ff1c3f56799d358605a26a44a
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-23 21:53:22 -0400

    savepoint

--- client/src/components/ResultsPanel.js
+++ client/src/components/ResultsPanel.js
@@ -2,17 +2,25 @@ import React from 'react';
 import {Result} from './index';
 
 const ResultsPanel = (props) => {
-  let results = props.resultSet.map((article, i) => {
-    return (
-      <Result 
-        article={article} 
-        resultSource={props.resultSource}
-        clickSave={props.clickSave} 
-        key={article._id}
-        index={i}
-      />
-    )
-  })
+  let results;
+  if(props.resultSet.length) {
+    results = props.resultSet.map((article, i) => {
+      return (
+        <Result 
+          article={article} 
+          resultSource={props.resultSource}
+          clickSave={props.clickSave} 
+          key={article._id}
+          index={i}
+        />
+      )
+    })
+  } else results = (
+    <div>
+      <span className='spin'>✰</span>
+      <span>Searching for results...</span>
+    </div>
+  )
 
   return (
     <section className='resultsPanel'>
