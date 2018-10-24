49f2c062d3d1c95e8ad2e9a627815ab0aafe54e0
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-23 21:48:53 -0400

    Updates

---
+++ precache-manifest.109e5db96d925f48434a59521adff03b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "072b0132ffce6e7b5134",
+    "url": "/nyt-search/static/js/runtime~main.072b0132.js"
+  },
+  {
+    "revision": "d2f236ca60573d41c622",
+    "url": "/nyt-search/static/js/main.d2f236ca.chunk.js"
+  },
+  {
+    "revision": "758fa75389c58713b665",
+    "url": "/nyt-search/static/js/1.758fa753.chunk.js"
+  },
+  {
+    "revision": "d2f236ca60573d41c622",
+    "url": "/nyt-search/static/css/main.1b6f206e.chunk.css"
+  },
+  {
+    "revision": "4889ff669891a757a4f24872c19ba988",
+    "url": "/nyt-search/index.html"
+  }
+];
