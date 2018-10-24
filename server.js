50fbac16609af8f8dcab93b63b0329416db7a58e
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-23 22:13:01 -0400

    savepoint

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+}  
+app.use(allowCrossDomain); // plumbing it in as middleware
+
 app.use(express.urlencoded({extended: true}));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
